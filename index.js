cd31fa5d467ab9e7ce920fa7176406629b9e0d61
nicoosommer <nicosomer388@gmail.com>
2023-10-25 21:22:56 -0300

    Verificacion finalizada. All ready

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const password = document.getElementById("password");
 const confirm = document.getElementById("confirm");
 const msjs = document.querySelectorAll(".mostrar");
-
-console.log(msjs);
+const form = document.querySelector("form");
+const boton = document.querySelector("button");
 
 function check() {
-  if (password.value === confirm.value) {
+  if (
+    password.value === confirm.value &&
+    password.value.length >= 8 &&
+    confirm.value.length >= 8
+  ) {
     msjs.forEach((msj) => {
       msj.classList.remove("mostrar");
       msj.classList.add("esconder");
     });
+    password.classList.remove("error");
+    confirm.classList.remove("error");
+  } else {
+    msjs.forEach((msj) => {
+      msj.classList.remove("esconder");
+      msj.classList.add("mostrar");
+    });
+    password.classList.add("error");
+    confirm.classList.add("error");
   }
 }
-password.addEventListener("click", check);
+function enviar() {
+  console.log(password.classList.contains("error"));
+  if (password.classList.contains("error") == false) {
+    form.submit();
+    return;
+  } else {
+    alert("Las contraseñas no coinciden");
+    return false;
+  }
+}
+password.addEventListener("input", check);
+confirm.addEventListener("input", check);
